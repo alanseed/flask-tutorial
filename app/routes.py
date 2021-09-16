07871300306880014de023a030cae76e3f485abb
alanseed <alan.seed@gmail.com>
2021-09-16 15:46:49 +1000

    tut 3 part 1

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,25 @@
-from app import app 
+from app.forms import LoginForm
+from flask import render_template 
+from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Hello, World!"  
+  user={'username':'Alan'}
+  posts = [ 
+    { 
+      'author': {'username':'John'},
+      'body':'Beatutiful day in Portland'
+    },
+    {
+      'author':{'username':'Susan'},
+      'body':'The avengers move was so cool'
+     }
+  ]
+  return render_template('index.html',title='Home',user=user, posts=posts)
+
+@app.route('/login')
+def login():
+  form = LoginForm()
+  return render_template('login.html',title='Sign In',form=form)
